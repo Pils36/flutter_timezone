306cc3db316a2c75cc5933b814660b45780dcf81
Adebambo Adenuga <adenugaadebambo41@gmail.com>
2020-07-10 02:10:56 +0100

    commit changes

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,96 @@
 
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Hello Adebambo!'),
-          centerTitle: false,
-          backgroundColor: Colors.blue[700],
-        ),
-        body: Center(
-          child: Text('Testing 123...'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.audiotrack),
-        ),
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Clock',
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  int _currentIndex = 0;
+
+  final tabs = [
+    Center(
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        color: Colors.blue[300],
+        width: 48.0,
+        height: 48.0,
       ),
-    ));
+    ),
+    Center(
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        color: Colors.blue[300],
+        width: 48.0,
+        height: 48.0,
+      ),
+    ),
+    Center(
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        color: Colors.blue[300],
+        width: 48.0,
+        height: 48.0,
+      ),
+    ),
+    Center(
+      child: Container(
+        margin: const EdgeInsets.all(10.0),
+        color: Colors.blue[300],
+        width: 48.0,
+        height: 48.0,
+      ),
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Clock'),
+        centerTitle: false,
+        backgroundColor: Colors.blue[700],
+      ),
+      body: tabs[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+              backgroundColor: Colors.blue[700]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Search'),
+              backgroundColor: Colors.blue[700]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.location_on),
+              title: Text('Location'),
+              backgroundColor: Colors.blue[700]),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              title: Text('Settings'),
+              backgroundColor: Colors.blue[700]),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
